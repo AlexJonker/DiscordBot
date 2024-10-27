9f2ec2940449cecfb1567c64fe737dbe7c021031
Alex Jonker <alex.jonker2008@gmail.com>
2024-10-27 11:17:31 +0100

    import json in the python file

--- bot.py
+++ bot.py
@@ -5,56 +5,45 @@ from os import *
 from itertools import cycle
 from dotenv import load_dotenv, dotenv_values 
 load_dotenv() 
-from configparser import ConfigParser
-config = ConfigParser()
-config.read('config.ini')
+from python_json_config import ConfigBuilder
 
-bot = commands.Bot(command_prefix=config.get('main', 'prefix'), intents = discord.Intents.all(), description='A simple example of bot made with Discord.py')
 
-bot.remove_command('help')
+builder = ConfigBuilder()
+
+config = builder.parse_config('config.json')
+
+
+
+
+bot = commands.Bot(command_prefix=config.prefix, intents = discord.Intents.all())
+
+bot.remove_command("help")
 
 
 async def load_cogs():
-    for cog in listdir('./cogs'):
-        if cog.endswith('.py'):
+    for cog in listdir("./cogs"):
+        if cog.endswith(".py"):
             try:
-                await bot.load_extension(f'cogs.{cog[:-3]}')
-                print(f'Loaded {cog}')
+                await bot.load_extension(f"cogs.{cog[:-3]}")
+                print(f"Loaded {cog}")
             except Exception as e:
-                print(f'Failed to load cog {cog}: {e}')
+                print(f"Failed to load cog {cog}: {e}")
 
 
 
 @bot.event
 async def on_ready():
     await load_cogs()
-    print(f'{bot.user.name} is online and ready!')
+    print(f"{bot.user.name} is online and ready!")
 
     change_status.start()
 
 
 
-statuslist = [
-    'Working in da fields',
-    'Doing stuff...',
-    'Browsing memes 🌐',
-    'Serving the finest pings',
-    'Counting binary sheep 🐑',
-    'In your server, stealing your bandwidth',
-    'Error 404: Status not found',
-    'Beep boop, I am alive',
-    'Running low on coffee ☕',
-    'Upgrading to Bot v2.0',
-    'Reading the lore 📖',
-    'Just vibin\' 😎',
-    'Calculating… still calculating…',
-    'With ur mom'
-]
-
 
 
 @tasks.loop(seconds=60)
 async def change_status():
-    await bot.change_presence(activity=discord.Game(random.choice(statuslist)))
+    await bot.change_presence(activity=discord.Game(random.choice(config.statuslist)))
 
 bot.run(getenv("TOKEN"))
