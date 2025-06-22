c16282cdb63f74c8c75d205f27aa552cce137cea
Alex Jonker <alex.jonker2008@gmail.com>
2025-06-22 22:37:15 +0200

    fix: jellyfin stream urls

--- cogs/music.py
+++ cogs/music.py
@@ -13,7 +13,7 @@ from async_timeout import timeout
 from discord.ext import commands
 
 # Silence useless bug reports messages
-yt_dlp.utils.bug_reports_message = lambda: ''
+yt_dlp.utils.bug_reports_message = lambda *a, **k: ''
 
 
 class VoiceError(Exception):
@@ -59,11 +59,12 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.uploader = data.get('uploader')
         self.uploader_url = data.get('uploader_url')
         date = data.get('upload_date')
-        self.upload_date = date[6:8] + '.' + date[4:6] + '.' + date[0:4]
+        self.upload_date = date[6:8] + '.' + date[4:6] + '.' + date[0:4] if date else "Unknown"
         self.title = data.get('title')
         self.thumbnail = data.get('thumbnail')
         self.description = data.get('description')
-        self.duration = self.parse_duration(int(data.get('duration')))
+        duration_val = data.get('duration')
+        self.duration = self.parse_duration(int(duration_val)) if duration_val is not None else "Unknown"
         self.tags = data.get('tags')
         self.url = data.get('webpage_url')
         self.views = data.get('view_count')
@@ -111,25 +112,6 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         # Single video case
         return cls(ctx, discord.FFmpegPCMAudio(data['url'], **cls.FFMPEG_OPTIONS), data=data)
-
-        webpage_url = process_info['webpage_url']
-        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
-        processed_info = await loop.run_in_executor(None, partial)
-
-        if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
-
-        if 'entries' not in processed_info:
-            info = processed_info
-        else:
-            info = None
-            while info is None:
-                try:
-                    info = processed_info['entries'].pop(0)
-                except IndexError:
-                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
-
-        return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
 
     @staticmethod
     def parse_duration(duration: int):
